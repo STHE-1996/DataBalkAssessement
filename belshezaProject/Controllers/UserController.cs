c23d1d6d8b56794f5a00460f39c4f96759531ded
sthem <sthebisob@ejoobi.co.za>
2023-06-12 16:07:29 +0200

    New commits

---
+++ belshezaProject/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using BelshezaProject.Data;
+using BelshezaProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace BelshezaProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _authContext;
+
+        public UserController(ApplicationDbContext applicationDbContext)
+        {
+            _authContext = applicationDbContext;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] User userObj)
+        {
+            if (userObj == null || string.IsNullOrEmpty(userObj.Email) || string.IsNullOrEmpty(userObj.Password))
+                return BadRequest();
+
+            var user = await _authContext.users
+                .FirstOrDefaultAsync(x => x.Email == userObj.Email && x.Password == userObj.Password);
+
+            if (user == null)
+                return NotFound(new { Message = "User Not Found!" });
+
+            return Ok(new
+            {
+                Message = "Successful Login!"
+            });
+        }
+        [HttpPost("regiter")]
+        public async Task<IActionResult> RegisterUser([FromBody] User userObj)
+        {
+            if(userObj == null)
+                return BadRequest();
+            
+            await _authContext.users.AddAsync(userObj);
+            await _authContext.SaveChangesAsync();
+            return Ok(new
+            {
+                Message = "User Registered!"
+            });
+          
+
+            
+        }
+
+
+
+    }
+}
---
+++ belshezaProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BelshezaProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BelshezaProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        } 
+
+        public DbSet<User> users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("users");
+        }
+
+    }
+}
